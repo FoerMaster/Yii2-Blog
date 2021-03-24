838f67e0d073415bbde088ac13f8314e046fd8f0
Foer <39239388+FoerMaster@users.noreply.github.com>
2021-03-24 15:43:53 +0500

    Удалил лишнее

--- web/js/ajaxcom.js
+++ web/js/ajaxcom.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
     $('#comForm').on('beforeSubmit', function() {
-        // Получаем объект формы
         var $testform = $(this);
-        // отправляем данные на сервер
         $.ajax({
-            // Метод отправки данных (тип запроса)
             type : $testform.attr('method'),
-            // URL для отправки запроса
             url : $testform.attr('action'),
-            // Данные формы
             data : $testform.serializeArray()
         }).done(function(data) {
 
             if (data.error == null) {
-                // Если ответ сервера успешно получен
+
 
                 var div = document.createElement("div");
                 div.innerHTML = data.model;
@@ -24,15 +19,15 @@ $(document).ready(function() {
                 console.log("Карточка создалась");
                 window.scrollTo(0,document.body.scrollHeight);
             } else {
-                // Если при обработке данных на сервере произошла ошибка
+
                 console.log(data.error);
             }
         }).fail(function() {
-            // Если произошла ошибка при отправке запроса
+
             $("#output").text("error3");
             console.log(data.error);
         })
-        // Запрещаем прямую отправку данных из формы
+
         return false;
     })
 })
